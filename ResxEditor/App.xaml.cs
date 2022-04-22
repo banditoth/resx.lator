adaff59f804d639091ca47542011b2a33074f2ee
András Tóth <andrastoth1126@outlook.com>
2022-04-22 11:09:22 +0200

    ResxEditor initial setup with .NET MAUI app

---
+++ ResxEditor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ResxEditor;
+
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ResxEditor/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ResxEditor;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
